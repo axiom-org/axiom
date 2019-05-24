66a1077000c58f1d77c3db13012ec39134ca5d35
Kevin Lacker <lacker@gmail.com>
2019-05-24 09:13:59 -0700

    bash hax to update version number

--- ts/build-extension.sh
+++ ts/build-extension.sh
@@ -6,6 +6,12 @@ then
     exit 1
 fi
 
+VERSION=$(grep '^ *\"version\":' ./package.json | head -1 | sed 's/^ *\"version\": \"//' | sed s/\",//)
+echo version: $VERSION
+
+sed -i 's/\(^ *\"version\": \"\).*\(\",\)$/\1MARKER\2/' ext-$NETWORK/manifest.json
+sed -i s/MARKER/$VERSION/ ext-$NETWORK/manifest.json
+
 parcel -p 2345 --no-hmr build \
        src/browser/popup-main.tsx \
        src/browser/background-main.ts \
