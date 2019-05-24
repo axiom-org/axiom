ea502fe4a04f2d61f35ba2798a0afb432ed300e5
Kevin Lacker <lacker@gmail.com>
2019-05-24 09:59:59 -0700

    production extension building OK

--- ts/build-extension.sh
+++ ts/build-extension.sh
@@ -12,14 +12,23 @@ echo version: $VERSION
 sed -i 's/\(^ *\"version\": \"\).*\(\",\)$/\1MARKER\2/' ext-$NETWORK/manifest.json
 sed -i s/MARKER/$VERSION/ ext-$NETWORK/manifest.json
 
-parcel -p 2345 --no-hmr build \
+if [ "$NODE_ENV" != "production" ]
+then
+    # Args only for live builds
+    ARGS="-p 2345 --no-hmr"
+fi
+
+parcel $ARGS build \
        src/browser/popup-main.tsx \
        src/browser/background-main.ts \
        src/browser/content-main.ts \
        src/browser/loader-main.ts \
        -d ext-$NETWORK/
 
-if [ "$NODE_ENV" == "production"]
+if [ "$NODE_ENV" == "production" ]
 then
     echo zipping extension files...
+    FILE=zips/extension-$NETWORK-$VERSION.zip
+    zip -r $FILE ext-$NETWORK
+    echo zip created at $FILE
 fi
