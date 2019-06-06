7c75b5a7e14844ed78a5c141e1ec874ddead21a1
Kevin Lacker <lacker@gmail.com>
2019-06-06 15:31:45 -0700

    dont use webtorrent debug

--- testnet/hserver-entrypoint.sh
+++ testnet/hserver-entrypoint.sh
@@ -8,7 +8,7 @@ echo ------------------------------ hserver-entrypoint.sh ----------------------
 KEYPAIR=`find /secrets/keypair | grep json | head -1`
 echo loading keypair: $KEYPAIR
 
-DEBUG=* node_modules/.bin/ts-node --project tsconfig.node.json \
+node_modules/.bin/ts-node --project tsconfig.node.json \
 	src/node/hserver-main.ts \
 	--keypair=$KEYPAIR \
 	--network=alpha \
