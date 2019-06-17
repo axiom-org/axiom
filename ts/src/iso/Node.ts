4a14aca7bb41f41c97429bb6223cfb7e7e11db8e
Kevin Lacker <lacker@gmail.com>
2019-06-17 14:02:54 -0700

    peers

--- ts/src/iso/Node.ts
+++ ts/src/iso/Node.ts
@@ -1,9 +1,13 @@
+import Peer from "./Peer";
+
 // A Node represents a member of the Axiom peer-to-peer network.
 export default class Node {
   verbose: boolean;
+  peers: Peer[];
 
   constructor(verbose: boolean) {
     this.verbose = verbose;
+    this.peers = [];
   }
 
   log(...args) {
