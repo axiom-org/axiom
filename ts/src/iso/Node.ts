68c948bc9f0033758872662df20f21f670107a1f
Kevin Lacker <lacker@gmail.com>
2019-06-18 15:42:26 -0700

    key node peers by public key

--- ts/src/iso/Node.ts
+++ ts/src/iso/Node.ts
@@ -3,11 +3,15 @@ import Peer from "./Peer";
 // A Node represents a member of the Axiom peer-to-peer network.
 export default class Node {
   verbose: boolean;
-  peers: Peer[];
+
+  // When a new peer connects, it is added to peers.
+  // When a peer disconnects, it is destroyed.
+  // A node should only store one Peer per public key.
+  peers: { [publicKey: string]: Peer };
 
   constructor(verbose: boolean) {
     this.verbose = verbose;
-    this.peers = [];
+    this.peers = {};
   }
 
   log(...args) {
