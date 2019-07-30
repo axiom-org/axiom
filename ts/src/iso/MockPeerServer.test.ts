cafe8ab7357971074e9f197f07ae379cba5700c2
Kevin Lacker <lacker@gmail.com>
2019-07-30 12:29:22 -0700

    check completeness

--- ts/src/iso/MockPeerServer.test.ts
+++ ts/src/iso/MockPeerServer.test.ts
@@ -15,6 +15,11 @@ test("MockPeerServer basics", async () => {
   let bootstrap = MockPeerServer.makeBootstrap(4);
   let servers = MockPeerServer.makeServers(bootstrap);
 
-  // TODO: test the servers make a complete graph
+  // The servers should form a complete graph
+  expect(servers[0].node.numPeers()).toBe(3);
+  expect(servers[1].node.numPeers()).toBe(3);
+  expect(servers[2].node.numPeers()).toBe(3);
+  expect(servers[3].node.numPeers()).toBe(3);
+
   // TODO: spin up a whole network
 });
