bbcf91903d42bfad39118523c97deb05ce95dd1f
Kevin Lacker <lacker@gmail.com>
2019-08-26 12:37:44 -0700

    some sample app

--- ts/src/browser/App.tsx
+++ ts/src/browser/App.tsx
@@ -3,51 +3,81 @@
 import * as React from "react";
 
 import AxiomAPI from "./AxiomAPI";
+import Database from "../iso/Database";
 import Node from "../iso/Node";
+import SignedMessage from "../iso/SignedMessage";
 
 export default function App() {
   let axiom = new AxiomAPI({ network: "alpha", verbose: true });
   let node = axiom.createNode();
+  let database = node.database("comments");
 
   return (
     <div className="App">
-      <Chat node={node} />
+      <Chat database={database} />
     </div>
   );
 }
 
-class Chat extends React.Component<{ node: Node }, { clicks: number }> {
-  node: Node;
+class Chat extends React.Component<
+  { database: Database },
+  { comments: { [key: string]: SignedMessage } }
+> {
+  database: Database;
 
-  constructor(props: { node: Node }) {
+  constructor(props: { database: Database }) {
     super(props);
 
-    this.node = props.node;
+    this.database = props.database;
     this.state = {
-      clicks: 0
+      comments: {}
     };
 
-    this.node.subscribe("clicks", (sender, data) => {
-      this.setState({ clicks: this.state.clicks + 1 });
+    this.database.load();
+
+    this.database.onMessage((sm: SignedMessage) => {
+      if (sm.message.type === "Delete") {
+        return;
+      }
+      let key = sm.signer + ":" + sm.message.id;
+      let newComments = { ...comments };
+      newComments[key] = sm;
+      this.setState({ comments: newComments });
     });
   }
 
   render() {
     return (
       <div>
-        <h1>P2P Click Counting</h1>
-        <p>
-          The count is {this.state.clicks}{" "}
-          {this.state.clicks === 1 ? "click" : "clicks"}.
-        </p>
-        <button
-          onClick={() => {
-            this.node.publish("clicks", "no data");
-          }}
-        >
-          Click me
-        </button>
+        <h1>P2P Chat Proof Of Concept</h1>
+        <CommentForm />
+        {this.state.comments.map((comment, index) => (
+          <p key={index}>{comment}</p>
+        ))}
       </div>
     );
   }
+}
+
+function CommentForm(props) {
+  let [comment, setComment] = useState("");
+
+  let handleSubmit = e => {
+    e.preventDefault();
+    alert(`submitting ${comment}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Comment:
+        <input
+          type="text"
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
