2641235b75df0c421802425ed1bc2655610c4b48
Kevin Lacker <lacker@gmail.com>
2019-10-24 10:13:33 -0700

    strip down deployment stuff

--- testnet/deploy.sh
+++ testnet/deploy.sh
@@ -23,31 +23,19 @@ fi
 CSERVER=cserver$1
 DEPLOYMENT=cserver$1-deployment
 HSERVER=hserver$1
-DB=db$1
 KEYPAIR=keypair$1
 VOLUME=volume$1
 CLAIM=claim$1
-
-CONNECTION_NAME=`gcloud sql instances describe $DB | grep connectionName | sed 's/connectionName: //'`
-
-if [ -z "$CONNECTION_NAME" ]; then
-    echo "could not find sql connection name"
-    exit 1
-fi
-
-# echo sql connection name: $CONNECTION_NAME
 
 kubectl delete deployment $DEPLOYMENT > /dev/null 2>&1
 
 sed s/PROJECT_ID/$PROJECT_ID/g ./deployment.yaml \
     | sed "s/cserverX/$CSERVER/g" \
     | sed "s/hserverX/$HSERVER/g" \
-    | sed "s/dbX/$DB/g" \
     | sed "s/keypairX/$KEYPAIR/g" \
     | sed "s/claimX/$CLAIM/g" \
     | sed "s/volumeX/$VOLUME/g" \
     | sed "s/DEPLOY_TIME/`date`/" \
-    | sed "s/CONNECTION_NAME/$CONNECTION_NAME/" \
     | kubectl apply -f -
 
 echo waiting for $DEPLOYMENT to be available...
