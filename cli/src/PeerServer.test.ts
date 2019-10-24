2d7e82038e778be9975dcf253d0387bfb3a2485c
Kevin Lacker <lacker@gmail.com>
2019-10-24 14:05:44 -0700

    jest works but afaict the open handle problem is real

---
+++ cli/src/PeerServer.test.ts
@@ -0,0 +1,49 @@
+import { KeyPair, Node, Peer } from "axiom-api";
+import PeerServer from "./PeerServer";
+
+process.on("unhandledRejection", error => {
+  console.log("unhandled rejection:", (error as any).stack);
+});
+
+test("PeerServer single connection", async () => {
+  let s = new PeerServer(null, 2222, true);
+  let clientPeer = Peer.connectToServer(null, "ws://localhost:2222", true);
+
+  let serverPeer: Peer = await new Promise((resolve, reject) => {
+    s.onPeer(peer => resolve(peer));
+  });
+  await serverPeer.waitUntilConnected();
+  await clientPeer.waitUntilConnected();
+});
+
+test.skip("PeerServer bootstrapping", async () => {
+  let urls = ["ws://localhost:2223"];
+  let verbose = false;
+
+  let kp1 = KeyPair.fromRandom();
+  let node1 = new Node(kp1, urls, verbose);
+
+  let node2 = new Node(null, urls, verbose);
+  let node3 = new Node(null, urls, verbose);
+
+  let server = new PeerServer(kp1, 2223, verbose);
+
+  server.connectNode(node1);
+
+  let check = () => {
+    expect(node1.numPeers()).toBeLessThan(3);
+    expect(node2.numPeers()).toBeLessThan(3);
+    expect(node3.numPeers()).toBeLessThan(3);
+  };
+  node1.onEveryMessage(check);
+  node2.onEveryMessage(check);
+  node3.onEveryMessage(check);
+
+  await node1.waitUntil(() => node1.numPeers() === 2);
+  await node2.waitUntil(() => node2.numPeers() === 2);
+  await node3.waitUntil(() => node3.numPeers() === 2);
+
+  node1.destroy();
+  node2.destroy();
+  node3.destroy();
+});
---
+++ cli/src/PeerServer.ts
@@ -0,0 +1,109 @@
+import * as http from "http";
+import * as WebSocket from "ws";
+const url = require("url");
+
+import { KeyPair, Node, Peer, Sequence } from "axiom-api";
+
+// A PeerServer listens for websockets and exchanges enough information over them
+// to construct a Peer connection.
+export default class PeerServer {
+  verbose: boolean;
+  peerHandler: (Peer) => void;
+  keyPair: KeyPair;
+  port: number;
+  node: Node;
+
+  constructor(keyPair: KeyPair, port: number, verbose: boolean) {
+    this.keyPair = keyPair;
+    if (!this.keyPair) {
+      this.keyPair = KeyPair.fromRandom();
+    }
+    if (!port || port < 1) {
+      throw new Error(`bad port: ${port}`);
+    }
+    this.port = port;
+    this.verbose = verbose;
+    this.peerHandler = null;
+    this.node = null;
+
+    let server = http.createServer((req, res) => {
+      let parsed = url.parse(req.url, true);
+      if (parsed.pathname === "/healthz") {
+        res.write("OK\n");
+        res.end();
+        return;
+      }
+
+      if (parsed.pathname === "/statusz") {
+        let status = this.status();
+        for (let line of status) {
+          res.write(line + "\n");
+        }
+        res.end();
+        return;
+      }
+    });
+
+    let wss = new WebSocket.Server({ server: server });
+    wss.on("connection", ws => {
+      let peer = new Peer({ keyPair: this.keyPair, verbose: verbose });
+
+      peer.signals.forEach(data => {
+        ws.send(JSON.stringify(data));
+      });
+
+      let incomingSignals = new Sequence<object>();
+      ws.on("message", encoded => {
+        try {
+          let signal = JSON.parse(encoded);
+          incomingSignals.push(signal);
+        } catch (e) {
+          console.log("websocket decoding error:", e);
+        }
+      });
+      peer.connect(incomingSignals);
+
+      if (this.peerHandler) {
+        this.peerHandler(peer);
+      }
+    });
+
+    server.listen(port);
+  }
+
+  log(...args) {
+    if (this.verbose) {
+      console.log(...args);
+    }
+  }
+
+  status(): string[] {
+    if (this.node) {
+      return this.node.statusLines();
+    }
+
+    return ["this.node == null"];
+  }
+
+  onPeer(callback: (Peer) => void) {
+    if (this.peerHandler) {
+      throw new Error("onPeer can only be called once");
+    }
+    this.peerHandler = callback;
+  }
+
+  // Let peers connect to the provided node through this PeerServer.
+  connectNode(node: Node) {
+    if (this.node) {
+      throw new Error("can only connectNode once");
+    }
+    this.node = node;
+    if (this.keyPair.getPublicKey() !== this.node.keyPair.getPublicKey()) {
+      throw new Error("keys from PeerServer and Node must match");
+    }
+    this.onPeer(async peer => {
+      await peer.waitUntilConnected();
+      this.node.addPeer(peer);
+    });
+  }
+}
