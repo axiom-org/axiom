56cdb0ae75290cccad800ae556c3c5ef46091bb1
Kevin Lacker <lacker@gmail.com>
2019-10-25 12:02:32 -0700

    remove stuff

--- ts/compile.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo tsc --build tsconfig.node.json
-tsc --build tsconfig.node.json || exit 1
-echo OK
-echo tsc --build tsconfig.browser.json
-tsc --build tsconfig.browser.json || exit 1
-echo OK
--- ts/files/README.md
+++
@@ -1,3 +0,0 @@
-This directory contains files that aren't readable source code.
-
-
